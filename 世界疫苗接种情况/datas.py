a6fd218f0b5bd39af3f1366252772f4063a1b07d
zhangmingyu1917645523 <138028641+zhangmingyu1917645523@users.noreply.github.com>
2023-06-28 23:03:18 +0800

    Add files via upload

---
+++ 世界疫苗接种情况/datas.py
@@ -0,0 +1,56 @@
+covid_19_data = [
+    {'name': 'Ukraine', 'value': [0.0, 5, 'Ukraine']},
+    {'name': 'Iceland', 'value': [7.6, 27801, 'Iceland']},
+    {'name': 'Gibraltar', 'value': [88.1, 29692, 'Gibraltar']},
+    {'name': 'Japan', 'value': [0.39, 490819, 'Japan']},
+    {'name': 'France', 'value': [5.4, 3604653, 'France']},
+    {'name': 'Chile', 'value': [24.3, 4643082, 'Chile']},
+    {'name': 'Germany', 'value': [5.9, 4910308, 'Germany']},
+    {'name': 'Israel', 'value': [53.6, 4921648, 'Israel']},
+    {'name': 'Russia', 'value': [3.7, 5401446, 'Russia']},
+    {'name': 'Brazil', 'value': [2.9, 6035017, 'Brazil']},
+    {'name': 'United Kingdom', 'value': [10.5, 6991310, 'United Kingdom']},
+    {'name': 'Turkey', 'value': [9.1, 7579623, 'Turkey']},
+    {'name': 'India', 'value': [0.93, 12709186, 'India']},
+    {'name': 'United States', 'value': [21.5, 70692645, 'United States']},
+    {'name': 'China', 'value': [11.8, 164470000, 'China']}
+]
+
+covid_19_data_ = [
+    {'name': 'China', 'value': [11.8, 164470000, 'China']},
+    {'name': 'Turkey', 'value': [9.1, 7579623, 'Turkey']},
+    {'name': 'India', 'value': [0.93, 12709186, 'India']},
+    {'name': 'France', 'value': [5.4, 3604653, 'France']},
+    {'name': 'Ukraine', 'value': [0.0, 5, 'Ukraine']},
+    {'name': 'Chile', 'value': [24.3, 4643082, 'Chile']},
+    {'name': 'United States', 'value': [21.5, 70692645, 'United States']},
+    {'name': 'Russia', 'value': [3.7, 5401446, 'Russia']},
+    {'name': 'Germany', 'value': [5.9, 4910308, 'Germany']},
+    {'name': 'Iceland', 'value': [7.6, 27801, 'Iceland']},
+    {'name': 'Gibraltar', 'value': [88.1, 29692, 'Gibraltar']},
+    {'name': 'Japan', 'value': [0.39, 490819, 'Japan']},
+    {'name': 'Israel', 'value': [53.6, 4921648, 'Israel']},
+    {'name': 'Brazil', 'value': [2.9, 6035017, 'Brazil']},
+    {'name': 'United Kingdom', 'value': [10.5, 6991310, 'United Kingdom']}
+]
+
+country_name_map = {
+    'Ukraine': '乌克兰',
+    'Iceland': '冰岛',
+    'Gibraltar': '直布罗陀',
+    'Japan': '日本',
+    'France': '法国',
+    'Chile': '智利',
+    'Germany': '德国',
+    'Israel': '以色列',
+    'Russia': '俄罗斯',
+    'Brazil': '巴西',
+    'United Kingdom': '英国',
+    'Turkey': '土耳其',
+    'India': '印度',
+    'United States': '美国',
+    'China': '中国',
+}
+
+# import random
+# random.shuffle(covid_19_data)  # 打乱顺序
